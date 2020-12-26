f1c01cba0be682c1d3f906ab2003aa59f90f68f1
Ahmad Reza <ahmadrezagghz666@gmail.com>
2020-12-26 14:38:24 +0330

    fix discounet icons , ...

---
+++ js/newScripts/customModal.js
@@ -0,0 +1,15 @@
+const customModal = document.querySelector('#customModal') ;
+const customModalBtn = customModal.querySelector('button') ;
+const customBlackFilter = document.querySelector('#blackFilter') ;
+// customModal.classList.add('show') ;
+// customBlackFilter.classList.add('show')
+// customModal.classList.remove('show') ;
+// customBlackFilter.classList.add('show')
+customModalBtn.addEventListener('click',e=>{
+    customModal.classList.remove('show') ;
+    customBlackFilter.classList.remove('show') ;
+})
+customBlackFilter.addEventListener('click',e=>{
+    customModal.classList.remove('show') ;
+    customBlackFilter.classList.remove('show') ;
+})
